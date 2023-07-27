57af4b7cb274341ce8deb93265733d5206cdf191
EkaterinaShamshina <nireta2009@yandex.ru>
2023-07-27 14:57:12 +0300

    swiper slider was added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles/main.scss'
+// import Swiper and modules styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+// import Swiper and modules styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+}),
