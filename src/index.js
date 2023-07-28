49f90196760eb732c79273ff3bf38863604f82be
EkaterinaShamshina <nireta2009@yandex.ru>
2023-07-28 11:22:05 +0300

    slider was corrected

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import './styles/main.scss'
-// import Swiper and modules styles
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import './styles/main.scss'
 
-// core version + navigation, pagination modules:
 import Swiper from 'swiper'
 import { Navigation, Pagination } from 'swiper/modules'
 
-var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 'auto',
+// eslint-disable-next-line no-unused-vars
+var swiper = new Swiper('.testimonialsSwiper', {
+  slidesPerView: 2.5,
   spaceBetween: 30,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  // configure Swiper to use modules
   modules: [Navigation, Pagination],
 })
